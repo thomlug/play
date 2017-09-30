9e5608c648a1104493da1ad38a991af9be17f712
thomas <thomlug@gmail.com>
2017-09-30 15:02:17 +1300

    Results page is now editable

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -8,22 +8,22 @@
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <router-link active-class="active" exact class="nav-item nav-link" to="home">Dashboard</a>
+              <router-link active-class="active" exact class="nav-item nav-link" to="home">Dashboard</router-link>
             </li>
             <li class="nav-item">
-              <router-link active-class="active" exact class="nav-item nav-link" to="members">Members</a>
+              <router-link active-class="active" exact class="nav-item nav-link" to="members">Members</router-link>
             </li>
             <li class="nav-item">
-              <router-link active-class="active" exact class="nav-item nav-link" to="teams">Teams</a>
+              <router-link active-class="active" exact class="nav-item nav-link" to="teams">Teams</router-link>
             </li>
             <li class="nav-item">
-              <router-link active-class="active" exact class="nav-item nav-link" to="competitions">Competitions</a>
+              <router-link active-class="active" exact class="nav-item nav-link" to="competitions">Competitions</router-link>
             </li>
             <li class="nav-item">
-              <router-link active-class="active" exact class="nav-item nav-link" to="marketing">Marketing</a>
+              <router-link active-class="active" exact class="nav-item nav-link" to="marketing">Marketing</router-link>
             </li>
             <li class="nav-item">
-              <router-link active-class="active" exact class="nav-item nav-link" to="fixtures">Fixtures/Results</a>
+              <router-link active-class="active" exact class="nav-item nav-link" to="fixtures">Fixtures/Results</router-link>
             </li>
           </ul>
         </div>
--- src/pages/Results.vue
+++ src/pages/Results.vue
@@ -1,22 +1,34 @@
 <template>
   <main-layout>
     <p>Result</p>
+    <button v-on:click="edit" v-if="!editable">Edit</button>
+    <button v-on:click="save" v-if="editable">Save</button>
     <dl class="dl-horizontal">
       <dt> Home Team</dt>
-      <dd> {{match.homeTeam}} </dd>
+      <dd> 
+        <span v-if="!editable">{{match.homeTeam}} </span>
+        <input v-if="editable" v-model="match.homeTeam"/>
+      </dd>
       <dt> Home Photo</dt>
       <dd> <img :src="match.homePhoto"/></dd>
-        <dt> Away Team</dt>
-      <dd> {{match.awayTeam}}</dd>
+      <dt> Away Team</dt>
+      <dd> 
+          <span v-if="!editable">{{match.awayTeam}} </span>
+        <input v-if="editable" v-model="match.awayTeam"/></dd>
       <dt> Away Photo</dt>
       <dd> <img :src="match.awayPhoto"/></dd>
         <dt> Date</dt>
-      <dd> {{match.date}}</dd>
+      <dd> 
+          <span v-if="!editable">{{match.date}} </span>
+        <input v-if="editable" v-model="match.date"/></dd>
 
         <dt> Ground</dt>
-      <dd> {{match.ground}}</dd>
-
+      <dd> 
+          <span v-if="!editable">{{match.ground}} </span>
+        <input v-if="editable" v-model="match.ground"/></dd>
     </dl>
+
+    
   </main-layout>
 </template>
 
@@ -30,6 +42,7 @@
     },
     data: function () {
       return {
+        editable: false,
         match:  {},
         matchId: this.$route.params.match_id
         }
@@ -41,8 +54,17 @@
             asObject: true
         }
       };
-    }
-     
-    
+    },
+    methods:{
+      edit: function(){
+          this.editable = true;
+      },
+      save: function(){
+          var item = {... this.match};
+          delete item['.key'];
+          db.ref('match/'+this.$route.params.match_id).set(item);
+          this.editable = false;
+      }
+    }   
   }
 </script>
