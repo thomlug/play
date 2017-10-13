d8f041964422780d1ab0b857799ca1a4724970f3
thomas <thomlug@gmail.com>
2017-10-13 22:46:12 +1300

    Added Subs card

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <main-layout>
     <div class="row">
-      <div v-for="card in cards" class="col-md-3">
+      <div v-for="card in cards" :key="card.Title" class="col-md-3">
         <div class="card">
           <div class="card-block">
             <h4 class="card-title">{{card.title}}</h4>
@@ -11,23 +11,79 @@
         </div>
       </div>
     </div>
+
+<div class="row">
+  <div class="card col-md-3">
+      <div class="card-block">
+        <h4 class="card-title">Subs</h4>
+        <div class="card-block">
+          <span v-for="player in players" :key="player.key">
+          <img :src="player.photo"></img>
+          <h5 class="card-title">{{player.first_name}}</h5>
+        </span>
+       </div>
+     </div>
+     </div>
+     </div>
+     </div>
   </main-layout>
 </template>
-
+  
+<style src="slick-carousel/slick/slick.css"></style>
 <script>
+  import {db} from '../firebase';
   import MainLayout from '../layouts/Main.vue'
-
+  import Slick from 'vue-slick';
+  
   export default {
     components: {
-      MainLayout
+      MainLayout,
+      Slick
     },
     data: function(){
-        return {cards: [
-        {title: 'Active Competitions', subtitle: 'In your organisation'},
-        {title: 'Revenue', subtitle: 'Per Competition'},
-        {title: 'Total Revenue To Date', subtitle: 'In your organisation from payments'},
-        {title: 'Outstanding Payments', subtitle: 'From players in your organisation'},
-      ]};
-    }
+        return {
+          cards: [
+            {title: 'Active Competitions', subtitle: 'In your organisation'},
+            {title: 'Revenue', subtitle: 'Per Competition'},
+            {title: 'Total Revenue To Date', subtitle: 'In your organisation from payments'},
+            {title: 'Outstanding Payments', subtitle: 'From players in your organisation'},
+          ],
+          slickOptions: {
+                        //options can be used from the plugin documentation
+                        slidesToShow: 4,
+                        infinite: true,
+                        accessibility: true,
+                        adaptiveHeight: false,
+                        arrows: true,
+                        dots: true,
+                        draggable: true,
+                        edgeFriction: 0.30,
+                        swipe: true
+                    },
+          players: {},  
+         
+      };
+    },
+    firebase: {
+      players:{
+          source: db.ref('player')
+      }
+    },
+    methods: {
+      next() {
+          this.$refs.slick.next();
+      },
+
+      prev() {
+          this.$refs.slick.prev();
+      },
+
+      reInit() {
+          // Helpful if you have to deal with v-for to update dynamic lists
+          this.$nextTick(() => {
+              this.$refs.slick.reSlick();
+          });
+      },
+    },
   }
 </script>
