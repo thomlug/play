cbe13ce0f3e35f7997056f6ad7fc8158b9fa5c8e
Menake Ratnayake <mrat278@aucklanduni.ac.nz>
2017-12-28 22:00:09 +1300

    Started competitions list and added icons

--- src/pages/Competitions.vue
+++ src/pages/Competitions.vue
@@ -1,18 +1,50 @@
 <template>
   <main-layout>
-    <div class="col-12">
-      <div class="row outer">
+    <div class="col-12 outer">
+      <div class="row">
+        <div class="col-12">
+          <div class="card card-inverse text-center top-buffer bottom-buffer banner">
+            <div class="card-block">
+              <h1>Club/League 1</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
         <div class="col-md-5 col-sm-12">
           <div class="col-12">
-            <div class="card">
-              asd
-              <br>
-              <br>
-              <br>
-              <br>
-              <br>
-              <br>
-              <br>
+            <div class="top-buffer">
+            </div>
+            <div class="card card-outer">
+              <div class="card-block">
+                <ul>
+                  <li v-for="n in 3">
+                    <div class="row aligner">
+                      <avatar :size="100"></avatar>
+                        <div>
+                          <h4>Comp {{ n }}</h4>
+                        </div>
+                        <div>
+                          <input class="input" type="text">
+                        </div>
+                        <div>
+                          <img class="edit-con" src="../assets/pencil.png">
+                        </div>
+                    </div>
+                  </li>
+                </ul>
+
+              </div>
+                <div class="card-footer">
+                  <div class="row aligner">
+                      <div class="new-icon">
+                        <img src="../assets/plus-circle.png" alt="">
+                      </div>
+                      <div class>
+                        <h3>Enter new comp</h3>
+                      </div>
+                  </div>
+                </div>
             </div>
           </div>
         </div>
@@ -21,7 +53,11 @@
             <div class="heading text-center">
               <h3>All fixtures this week</h3>
             </div>
-            <fixtures></fixtures>
+            <div class="card card-outer">
+              <div class="card-block">
+                <fixtures></fixtures>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -36,12 +72,14 @@
   import Slick from 'vue-slick';
   import moment from 'moment'
   import Fixtures from '../components/Fixtures.vue'
+  import Avatar from '../components/Avatar.vue'
 
   export default {
     components: {
       MainLayout,
       Slick,
-      Fixtures
+      Fixtures,
+      Avatar
     }
   }
 </script>
@@ -55,6 +93,58 @@
   .card {
     max-height: 600px;
   }
+
+  .top-buffer{
+    margin-top: 37px;
+  }
+
+  .bottom-buffer{
+    margin-bottom: 30px;
+  }
+
+  .card-outer{
+    overflow-y: scroll;
+    overflow: auto;
+    overflow-x: hidden;
+  }
+
+  .aligner {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    /* padding: 5px; */
+  }
+
+  .aligner > div {
+    flex: 1 1 auto;
+    margin: 10px;
+  }
+
+  .input{
+    background-color: #1E88E5;
+  }
+
+  .banner{
+    background-color: #2E2F30;
+    border-color: #333;
+    height: 150px;
+  }
+
+  .edit-icon{
+    width: 10px;
+    height: 10px;
+  }
+
+  li {
+    margin: 0;
+    padding: 0.2em;
+    list-style-type: none;
+  }
+
+  ul{
+    overflow: auto;
+  }
+
 
 
 </style>
