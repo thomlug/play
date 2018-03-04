741c5f15e5b2537d7749e40fd74beced46ddf881
Menake Ratnayake <mrat278@aucklanduni.ac.nz>
2018-03-04 16:19:23 +1300

    Completed sign up and log in

--- src/components/PlayForm.vue
+++ src/components/PlayForm.vue
@@ -1,19 +1,15 @@
 <template>
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-4 col-sm-4 col-xs-12"></div>
-            <div class="col-md-4 col-sm-4 col-xs-12">
-                <div class="centered-col">
-                    <img src="https://firebasestorage.googleapis.com/v0/b/play-14e3e.appspot.com/o/logo.jpg?alt=media&token=1c35a16a-5311-4c41-a91d-a264505a71a8" class="logo">
-                    <div class="card form-container">
-                        <div class="card-body">
-                            <slot></slot>   
-                        </div>
-                    </div>
-                </div>    
+        <div class="centered-col">
+            <img src="https://firebasestorage.googleapis.com/v0/b/play-14e3e.appspot.com/o/logo.jpg?alt=media&token=1c35a16a-5311-4c41-a91d-a264505a71a8" class="logo">
+            <slot name="page-header"></slot>
+            <div class="card form-container">
+                <div class="card-body">
+                    <slot></slot>   
+                </div>
             </div>
-            <div class="col-md-4 col-sm-4 col-xs-12"></div>
-        </div>
+            <slot name="additional-info"></slot>
+        </div>                    
     </div>
 </template>
 
@@ -27,6 +23,7 @@ export default {
     .logo {
         border-radius: 50%;
         height: 4rem;
+        margin: 5vh 0;
     }
 
     .centered-col{
@@ -38,7 +35,8 @@ export default {
     }
 
     .form-container{
-        width: 40rem;
+        margin-top: 5vh;
+        width: 20rem;
         padding: 2rem 0 2rem 0;
         -webkit-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
         -moz-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
--- src/pages/Signup.vue
+++ src/pages/Signup.vue
@@ -1,14 +1,32 @@
 <template>
-  <play-form>
-    <div class="col">
-        <form @submit.prevent="onSignUp" class="login-form">
-            <input placeholder="Email" label="Email" id="email" type="email" class="email form-element text-center" v-model="email" required>
-            <input placeholder="Password" label="Password" id="password" type="password" class="password form-element text-center" v-model="password" required>
-            <input placeholder="Confirm Password" label="Confirm Password" id="confirmPassword" type="password" class="confirmPassword form-element text-center" v-model="confirmPassword">
-            <button type="submit" class="submit-button btn btn-submit form-element">Sign Up</button>
-        </form>  
-    </div>            
-  </play-form>
+    <div class="signup-page">
+        <div class="signup-page-content">
+            <div class="signup-page-block signup-page-registerBlock">
+                <play-form>
+                        <div class="col">
+                            <form @submit.prevent="onSignUp" class="register-form">
+                                <h4>Sign Up</h4>
+                                <input placeholder="Email" label="Email" id="email" type="email" class="email form-element text-center" v-model="email" required>
+                                <input placeholder="Password" label="Password" id="password" type="password" class="password form-element text-center" v-model="password" required>
+                                <input placeholder="Confirm Password" label="Confirm Password" id="confirmPassword" type="password" class="confirmPassword form-element text-center" v-model="confirmPassword">
+                                <button type="submit" class="submit-button btn btn-submit form-element">Sign Up</button>
+                            </form>  
+                        </div>            
+                    </play-form>
+            </div>
+    
+            <div class="signup-page-block signup-page-banner">
+                <div class="signup-page-bannerContent">
+                    <div class="signup-page-bannerTitle text-center">
+                        <p>Changing the way you play sport.</p>
+                    </div>
+                    <div class="signup-page-bannerMotto text-center">
+                        <p>SPORTS NETWORK. TEAM MANAGEMENT. CLUB MANAGEMENT. LIVE</p>
+                    </div>
+                </div>
+            </div> 
+        </div>  
+    </div>
 </template>
 
 <script>
@@ -27,17 +45,29 @@ export default {
 
     methods: {
         onSignUp(){
-            firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+            if (this.password === this.confirmPassword){
+                firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
                 .then(
                     user => {
-                        
+                        //Functionality on success.
+                        firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+                            .then(
+                                user => {
+                                    this.$router.push('/home');
+                                }
+                            )
                     }
                 )
                 .catch(
                     error => {
-                        console.log(error);
+                        //handle errors here
+                        alert('Oops ' + error.message)
                     }
                 )
+            }else {
+                alert('Passwords do not match');
+            }
+            
         }
     }
 }
@@ -45,7 +75,38 @@ export default {
 
 
 <style scoped>
-    .login-form{
+
+    .signup-page{
+        display: flex;
+        flex-direction: column;
+        min-height: 46rem;
+        margin-bottom: 0;
+        overflow: hidden;
+    }
+
+    .signup-page-content{
+        display: flex;
+        flex-direction: row-reverse;
+        flex-grow: 1;
+        flex-wrap: wrap;
+        align-items: stretch;
+    }
+
+    .signup-page-block{
+        display: flex;
+        flex: 1;
+        justify-content: center;
+    }
+
+    .signup-page-registerBlock{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+
+    .register-form{
         display: flex;
         align-items: center;
         justify-content: center;
@@ -59,5 +120,28 @@ export default {
 
     .submit-button:hover{
         cursor: pointer;
+    }
+
+    .signup-page-banner{
+        background-color: #212121;
+        overflow: hidden;
+        position:relative;
+    }
+
+    .signup-page-bannerContent{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .signup-page-bannerTitle{
+        font-size: 2rem;
+        color: #ffffff;
+    }
+
+    .signup-page-bannerMotto{
+        font-size: 1rem;
+        color: #2BCAD0;
     }
 </style>
