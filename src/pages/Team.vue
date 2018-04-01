eedca8930f16533a1f32036cd40a10819feb8b3d
PlayAppLtd <play_app@outlook.com>
2018-04-01 22:23:15 +1200

    Team - copied over latest code

--- src/pages/Team.vue
+++ src/pages/Team.vue
@@ -3,40 +3,95 @@
     <div class="row">
       <div class="col-md-3 col-sm-3 col-xs-12"></div>
       <div class="col-md-6 col-sm-6 col-xs-12">
-        <div class="team-profile-box">
-          <div class="team-profile-banner text-center">
-            <h1>Team Profile</h1>
-          </div>
-          <div class="team-profile-header">
-              <div v-if="canEditProfile()">
-                <img src="../assets/pencil.png" class="edit-icon" @click="edit" v-if="!editable">
-                <button class="btn btn-success" v-on:click="save" v-if="editable">Save</button>
-              </div>
-            </div>  
-          <div class="team-profile-content">            
-            <img class="content-block profile-photo" :src="team.photo"/>
-            <span class="content-block" v-if="!editable"><h4>{{team.name}}</h4> </span>
-            <input class="content-block" v-if="editable" v-model="team.name"/>
+        <div class="player-profile-box">
+          <div class="player-profile-banner text-center">
+            <h1>Player Profile</h1>
+          </div>          
+            <div class="player-profile-header" v-if="canEditProfile()">
+              <img src="../assets/pencil.png" class="edit-icon" @click="edit" v-if="!editable">
+              <button class="btn btn-success" @click="save" v-if="editable">Save</button>
+            </div>
 
-            <span class="content-block player-number" v-if="!editable">{{team.numberOfPlayers}} players </span>
-            <input class="content-block" v-if="editable" v-model="team.numberOfPlayers"/> 
-            <template v-if = "editable">
-              <h5 class="content-block" > Upload new photo </h5>
-              <form class="content-block" enctype="multipart/form-data" novalidate>
-                <div class="dropbox">
-                  <input type="file" :name="uploadFieldName" :disabled="isSaving" @change="filesChange" accept="image/*" class="input-file">
-                    <p v-if="isSaving">
-                      Uploading file...
-                    </p>
-                </div>
-              </form>
+            <div class="player-profile-header-other">
+            </div>
+
+            <div class="player-profile-content">
+              <img class="profile-photo" :src="player.photo" :class="'player-' + player.availability"/> 
+              <div class="content-block">
+                
+              <div class= "input-header" v-if="editable" >
+              <h7 class= "input-header" > Name</h7>
+              </div> 
+                <span class="text-center" v-if="!editable">
+                  <h4>{{player.first_name}} {{player.last_name}}</h4>
+                </span>
+              
+                <input placeholder="First name"  v-if="editable" v-model="player.first_name"/>
+                <input placeholder="Last name" v-if="editable" v-model="player.last_name"/>
+              </div>
+
+              <!-- <span v-if="!editable">{{player.age}}</span>
+              <input v-if="editable" v-model="player.age"/> -->
+              <div class="content-block profile-info" v-if="!editable">
+                <div>{{player.availability | camelToSentence}}</div>
+                <div><small>Last Updated {{moment(player.availabilityUpdated).calendar()}}</small></div>
+              </div>
+
+              <div class="content-block">
+              <div class= "input-header" v-if="editable" >
+              <h7 class= "input-header" > Preferred Position</h7>
+              </div> 
+                <span class="profile-info" v-if="!editable">{{player.preffered_position}}</span>
+                <input placeholder="Preffered position" v-if="editable" v-model="player.preffered_position"/>
+              </div>
+
+              <span class="content-block">
+              <div class= "input-header" v-if="editable" >
+              <h7 class= "input-header" > Phone Number</h7>
+              </div>
+                <i class="fa fa-phone"></i> 
+                <a  v-if="!editable" :href="'tel:'+player.phone">{{player.phone}} </a>
+                <input placeholder="Phone number"  v-if="editable" v-model="player.phone"/>
+              </span>
+
+              <span class="content-block">
+              <div class= "input-header" v-if="editable" >
+              <h7 class= "input-header" > Email</h7>
+              </div>
+                <i class="fa fa-envelope"></i>
+                <a  v-if="!editable" :href="'mailto:'+player.email">{{player.email}} </a>
+                <input placeholder="Email" v-if="editable" v-model="player.email"/>
+              </span>
+
+              <template v-if = "editable">
+                <form enctype="multipart/form-data" novalidate>
+                  <div class="dropbox">
+                    <input type="file" :name="uploadFieldName" :disabled="isSaving" @change="filesChange" accept="image/*" class="input-file">
+                      <p v-if="isSaving">
+                        Uploading file...
+                      </p>
+                  </div>
+                </form>
               </template>
+              <div v-if="canEditProfile()">
+                <h4 class="card-title">Update your status </h4>
+                <div class="availability-container">
+                  <button v-on:click="setCurrentPlayerAvailability('available')" type="button" class="btn btn-primary btn-available">Available</button>
+                  <button v-on:click="setCurrentPlayerAvailability('unavailable')" type="button" class="btn btn-danger">Unavailable</button>
+                </div>
+              </div>
+            </div>                       
+            
           </div>
         </div>
-
-      </div>
       <div class="col-md-3 col-sm-3 col-xs-12"></div>
     </div>
+
+<!-- test -->
+      
+
+  
+</div>
 
   </main-layout>
 </template>
@@ -44,6 +99,7 @@
 <script>
   import {db} from '../firebase';
   import MainLayout from '../layouts/Main.vue'
+  import moment from 'moment'
   const STATUS_INITIAL = 0, STATUS_SAVING = 1, STATUS_SUCCESS = 2, STATUS_FAILED = 3;
 
   export default {
@@ -76,8 +132,8 @@
     data: function () {
       return {
         editable: false,
-        team:  {},
-        teamId: this.$route.params.team_id,
+        player:  {},
+        playerId: this.$route.params.player_id,
         uploadedFiles: [],
         uploadError: null,
         currentStatus: null,
@@ -87,8 +143,8 @@
     },
     firebase() {
       return{
-        team:{
-            source: db.ref('team/'+this.$route.params.team_id),
+        player:{
+            source: db.ref('player/'+this.$route.params.player_id),
             asObject: true
         }
       };
@@ -99,16 +155,20 @@
       },
       canEditProfile: function(){
         var currentUser = this.currentUser();
-        return this.team != null &&  currentUser != null;
+        return this.player != null &&  currentUser != null && (this.player.userUid === currentUser.uid);
       },
       edit: function(){
           this.editable = true;
       },
       save: function(){
-          var item = {... this.team};
+          var item = {... this.player};
           delete item['.key'];
-          db.ref('team/'+this.$route.params.team_id).set(item);
+          db.ref('player/'+this.$route.params.player_id).set(item);
           this.editable = false;
+      },
+      setCurrentPlayerAvailability(availability){
+        this.$firebaseRefs.player.child('availabilityUpdated').set(this.moment().toString());        
+        this.$firebaseRefs.player.child('availability').set(availability);
       },
       reset() {
         // reset form to initial state
@@ -140,9 +200,9 @@
         this.saveFile(files[0]);
       },
       upload(fileData){
-          var team = this.team;
+          var player = this.player;
           var storageRef = firebase.storage().ref();
-          var testUploadRef = storageRef.child('images/' + team['.key']);
+          var testUploadRef = storageRef.child('images/' + player['.key']);
           var uploadTask = testUploadRef.put(fileData);
           var promise = new Promise((resolve, reject) => {
 
@@ -172,7 +232,7 @@
               }, function() {
               // Upload completed successfully, now we can get the download URL
                 var downloadURL = uploadTask.snapshot.downloadURL;
-                team.photo = downloadURL;
+                player.photo = downloadURL;
                 resolve(downloadURL);
               });
           });
@@ -186,28 +246,45 @@
   }
 </script>
 
-<style>
+<style scoped>
+
+
+
 .profile-photo{
     max-height:256px;
     max-width:256px;
     border-radius: 50%;
+    box-shadow: 3px 3px 3px -3px rgba(0,0,0,0.75);
 }
 
-.team-profile-box{
-  margin-top: 5vh;
+.player-available{
+    border: 2px solid #2acad0;
+}
+
+.player-unavailable{
+    border: 2px solid red;
+}
+
+.player-unknown{
+    border: 2px solid grey;
+    -webkit-filter: grayscale(100%);
+}
+
+.player-profile-box{
+  margin-top: 1vh;
   display: flex;
   flex-direction: column;
-  box-shadow: 2px 4px 31px -3px rgba(128,126,128,1);
+  box-shadow: 3px 3px 3px -3px rgba(128,126,128,1);
 }
 
-.team-profile-banner{
+.player-profile-banner{
   width: 100%;
   background-color: #2BCAD0;
   color: #ffffff;
 }
 
-.team-profile-header{
-  margin: 1rem 2.5rem;
+.player-profile-header{
+  margin: .5rem 2.5rem;
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
@@ -215,29 +292,47 @@
   float:right;
 }
 
-.team-profile-content{
+.player-profile-header-other{
+margin-top: 1rem;
+}
+
+.player-profile-content{
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
   text-align: center;
-  margin-bottom: 0rem;
+  margin-bottom: 2rem;
+  margin-top: 0rem;
 }
 
-img{
-   margin-bottom: 0rem;
+.availability-container{
+  display: block;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
 }
 
 .content-block{
   margin: 0.4rem 0;
 }
 
-.player-number{
-  color:#BDBDBD;
+.player-{
+  margin-top: 2rem;
+}
+
+.profile-info{
+  color: #9E9E9E;
+  font-weight: 400;
+}
+
+.profile-info small{
+  font-size:70%;
+  margin-top:-2px;
 }
 
 .edit-icon{
-  height:3rem;
+  height:2.25rem;
   border-radius: 20%;
   border: none;
   background: none;
@@ -245,8 +340,7 @@ img{
 
 .edit-icon:hover{
   cursor: pointer;
-  border: lightgrey;
-  border-width: 3px;
+  background: lightgrey;
   color: white;
 }
 
@@ -265,6 +359,38 @@ img{
   border: none;
   border-radius: 0;
   cursor: pointer;
+  -webkit-box-shadow: 3px 3px 3px -3px #50575e;
+}
+
+.btn-available{
+  background-color: #2bcad0;
+  border-color: #2bcad0;
+  border-radius: 0;
+  cursor: pointer;
+  -webkit-box-shadow: 3px 3px 3px -3px #50575e;
+}
+
+.btn-available:hover{
+  background-color: #26bec4;
+  border-color: #26bec4;
+  border-radius: 0;
+  cursor: pointer;
+  -webkit-box-shadow: 3px 3px 3px -3px #50575e;
+}
+
+.btn-danger{
+  background-color: #d9534f;
+  border-color: #d9534f;
+  cursor: pointer;
+  border-radius: 0;
+  -webkit-box-shadow: 3px 3px 3px -3px #50575e;
+}
+
+.btn-danger:hover{
+  background-color: #c44743;
+  border-color: #c44743;
+  cursor: pointer;
+  border-radius: 0;
   -webkit-box-shadow: 3px 3px 3px -3px #50575e;
 }
 
@@ -293,20 +419,17 @@ input{
   text-align: center;
 }
 
-.fixture-title{
-  margin-bottom: 0.75rem;
-}
-
 /* header styles */
 a{
   color: #0275d8;
   cursor: pointer;
-  font-weight:300;
+  font-weight: none;
 }
 
-span{
-  font-weight: 400;
+small{
+  font-weight: 300;
 }
+
 
 h1{
   text-transform:uppercase;
@@ -314,13 +437,16 @@ h1{
   margin-top: 0.25rem;
   font-size: 2rem;
   font-weight: 550;
-
-}
-h4{
-  font-weight: 550;
-  color: #50575e;
-  margin-bottom: 0rem;
-  text-align: none;
 }
 
+h7{
+  font-size: 0.8rem;
+  float: left;
+  margin-left: -0rem;
+  color: lightgray;
+}
+
+i{
+  color: lightgray;
+}
 </style>
