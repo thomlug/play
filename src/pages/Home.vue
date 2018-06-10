b8846857981786b8b8d14f69ff8fbe80bd118e76
thomlug <thomlug@gmail.com>
2018-06-10 16:32:24 +1200

    Fixed bug with teams object when signing up a new player (#102)

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -858,7 +858,8 @@ export default {
       var teamKey = this.getCurrentTeamKey();
       this.newPlayer[teamKey] = 1;
       this.newPlayer.teamKey = teamKey;
-      this.newPlayer.teams = {teamKey: {'teamKey': teamKey, 'position': [0,0]}};
+      this.newPlayer.teams = {};
+      this.newPlayer.teams[teamKey] = {'teamKey': teamKey, 'position': [0,0]};
       this.newPlayer.signUpPage = window.location.origin + '/#/join/';
       this.newPlayer.position = [0, 0];
       this.newPlayer.signUpToken = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
