81d8f9307ee03a6185d22898e3f9b2e651187281
PlayAppLtd <play_app@outlook.com>
2018-09-16 16:06:47 +1200

    Re-enabled facebook login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -62,8 +62,8 @@ export default {
       signInSuccessUrl: this.$route.query.redirect || "#/home",
       signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID
-        //firebase.auth.FacebookAuthProvider.PROVIDER_ID
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID
       ],
       // Terms of service url.
       tosUrl: "<your-tos-url>"
